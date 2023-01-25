f69b6b62d9c1a1fdffee20c47850bb6df5c957ec
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-25 11:22:35 +0530

    Add tags colors in noteItem

--- src/components/Notes/NoteItem.js
+++ src/components/Notes/NoteItem.js
@@ -1,9 +1,20 @@
 const NoteItem = ({ note }) => {
+  const colors = ['primary', 'danger', 'dark']
+  let random = Math.floor(Math.random() * colors.length)
+  console.log(random)
   return (
     <div className='col-md-3'>
       <div className='card my-2'>
         <div className='card-body'>
-          <h5 className='card-title'>{note.title}</h5>
+          <h5 className='card-title d-flex align-items-center'>
+            {note.title}
+            <span
+              className={`badge rounded-pill bg-${colors[random]} ms-2`}
+              style={{ fontSize: '0.7rem' }}
+            >
+              {note.tag}
+            </span>
+          </h5>
           <p className='card-text'>{note.description}</p>
           <button className='btn btn-info'>
             <i className='fa-regular fa-pen-to-square'></i>
