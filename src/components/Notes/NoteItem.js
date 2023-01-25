8e477f35f1a81421b5a766df250f35cf377c84db
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-25 19:02:35 +0530

    Add more colors for tag

--- src/components/Notes/NoteItem.js
+++ src/components/Notes/NoteItem.js
@@ -6,14 +6,30 @@ const NoteItem = ({ note }) => {
   const context = useContext(NoteContext)
   const { deleteNote } = context
 
-  const [random, setRandom] = useState(null);
-  const colors = ['primary', 'danger', 'dark']
+  const [random, setRandom] = useState(null)
+  const colors = [
+    '#28A745',
+    '#228B22',
+    '#008000',
+    '#008B8B',
+    '#3F00FF',
+    '#007BFF',
+    '#0437F2',
+    '#0000FF',
+    '#7B68EE',
+    '#DC3545',
+    '#880808',
+    '#D70040',
+    '#FF3131',
+    '#FF0000',
+    '#800000',
+  ]
 
   useEffect(() => {
     setRandom(Math.floor(Math.random() * colors.length))
     // eslint-disable-next-line
-  }, []);
-  
+  }, [])
+
   return (
     <div className='col-md-3'>
       <div className='card my-2'>
@@ -21,8 +37,13 @@ const NoteItem = ({ note }) => {
           <h5 className='card-title d-flex align-items-center'>
             {note.title}
             <span
-              className={`badge rounded-pill bg-${colors[random]} ms-2`}
-              style={{ fontSize: '0.7rem' }}
+              className={`badge rounded-pill ms-2`}
+              style={{
+                fontSize: '0.7rem',
+                backgroundColor: `${
+                  note.tag === 'General' ? '#000' : colors[random]
+                }`,
+              }}
             >
               {note.tag}
             </span>
